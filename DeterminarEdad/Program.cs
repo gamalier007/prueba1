d5c78a933af2a021a9846e6cb1b4a7a12551957d
gamalier007 <166254793+gamalier007@users.noreply.github.com>
2025-03-18 17:58:23 -0400

    Add files via upload

---
+++ DeterminarEdad/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DeterminarEdad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Edad de la persona");
+            Console.WriteLine();
+
+            int edad;
+
+            Console.Write("Introduzca la edad de la persona: ");
+            edad=Convert.ToInt32(Console.ReadLine());
+
+            if (edad >= 18)
+            {
+                Console.WriteLine("ES mayor de edad.");
+            }
+            else {
+                Console.WriteLine("Es menor de edad.");
+            }
+
+        }
+    }
+}
